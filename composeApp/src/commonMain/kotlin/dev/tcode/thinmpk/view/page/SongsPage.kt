fc05e877115d523490a125fd489f03cc0813a91c
tcode <tk@tcode.dev>
2026-02-25 18:56:24 +0900

    add SongsPage composable with top app bar layout

---
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/page/SongsPage.kt
@@ -0,0 +1,30 @@
+package dev.tcode.thinmpk.view.page
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SongsPage() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Songs") }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+        }
+    }
+}
