3f6b7aab763e7b3d15494ceb946188da8a2a7fac
tcode <tk@tcode.dev>
2026-03-12 12:54:05 +0900

    implement iOS song fetching in SongRepository using MPMediaQuery

--- composeApp/src/iosMain/kotlin/dev/tcode/thinmpk/repository/SongRepository.kt
+++ composeApp/src/iosMain/kotlin/dev/tcode/thinmpk/repository/SongRepository.kt
@@ -1,10 +1,32 @@
 package dev.tcode.thinmpk.repository
 
 import dev.tcode.thinmpk.model.SongModel
+import platform.MediaPlayer.MPMediaItem
+import platform.MediaPlayer.MPMediaItemPropertyIsCloudItem
+import platform.MediaPlayer.MPMediaPropertyPredicate
+import platform.MediaPlayer.MPMediaQuery
 
 actual class SongRepository actual constructor() {
     actual fun findAll(): List<SongModel> {
-        // TODO: Implement iOS song fetching using MPMediaQuery
-        return emptyList()
+        val query = MPMediaQuery.songsQuery()
+        val predicate = MPMediaPropertyPredicate.predicateWithValue(
+            value = false,
+            forProperty = MPMediaItemPropertyIsCloudItem
+        )
+        query.addFilterPredicate(predicate)
+
+        val items = query.items ?: return emptyList()
+
+        return items.mapNotNull { item ->
+            val mediaItem = item as? MPMediaItem ?: return@mapNotNull null
+            SongModel(
+                id = mediaItem.persistentID.toString(),
+                name = mediaItem.title ?: "",
+                artistName = mediaItem.artist ?: "",
+                albumId = mediaItem.albumPersistentID.toString(),
+                albumName = mediaItem.albumTitle ?: "",
+                duration = mediaItem.playbackDuration.toInt()
+            )
+        }
     }
 }
