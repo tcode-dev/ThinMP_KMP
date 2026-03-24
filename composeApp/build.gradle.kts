af902b10076e2e18e52a68d8925ba79fd445ec08
tcode <tk@tcode.dev>
2026-03-25 08:27:26 +0900

    Add Koin Compose dependency to build configuration

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -42,6 +42,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.navigation.compose)
             implementation(libs.koin.core)
+            implementation(libs.koin.compose)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
