1b7994a3405ccc4911c829cf80ab6df79ce84282
tcode <tk@tcode.dev>
2026-04-01 12:33:35 +0900

    Refactor ArtworkImage composable to use AsyncImage for loading artwork

--- composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/component/image/ArtworkImage.kt
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/component/image/ArtworkImage.kt
@@ -1,23 +1,15 @@
 package dev.tcode.thinmpk.view.component.image
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import dev.tcode.thinmpk.bridge.ArtworkBridge
-import org.koin.compose.koinInject
+import coil3.compose.AsyncImage
+import dev.tcode.thinmpk.model.ArtworkModel
 
 @Composable
 fun ArtworkImage(
@@ -25,26 +17,15 @@ fun ArtworkImage(
     contentDescription: String?,
     modifier: Modifier = Modifier,
     size: Dp = 50.dp,
-    artworkBridge: ArtworkBridge = koinInject(),
 ) {
-    var artwork by remember(imageId) { mutableStateOf<ImageBitmap?>(null) }
+    val placeholder = ColorPainter(MaterialTheme.colorScheme.surfaceVariant)
 
-    LaunchedEffect(imageId) {
-        artwork = artworkBridge.getArtwork(imageId)
-    }
-
-    if (artwork != null) {
-        Image(
-            bitmap = artwork!!,
-            contentDescription = contentDescription,
-            modifier = modifier.size(size),
-            contentScale = ContentScale.Crop,
-        )
-    } else {
-        Box(
-            modifier = modifier
-                .size(size)
-                .background(MaterialTheme.colorScheme.surfaceVariant),
-        )
-    }
+    AsyncImage(
+        model = ArtworkModel(id = imageId),
+        contentDescription = contentDescription,
+        modifier = modifier.size(size),
+        contentScale = ContentScale.Crop,
+        placeholder = placeholder,
+        error = placeholder,
+    )
 }
