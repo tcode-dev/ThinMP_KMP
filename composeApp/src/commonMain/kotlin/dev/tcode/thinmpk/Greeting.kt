a78ed0052ced489351ad300d9c2ce755dceff7b7
tcode <tk@tcode.dev>
2026-04-07 08:52:10 +0900

    Remove platform-specific implementations for Android and iOS

--- composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.tcode.thinmpk
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/Platform.kt
+++
@@ -1,7 +0,0 @@
-package dev.tcode.thinmpk
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
