bb2e4f433d50f5f8c76365b823c5b0ebe6934403
tcode <tk@tcode.dev>
2026-04-24 18:38:01 +0900

    Implement AlbumRepositoryImpl for managing album data retrieval

---
+++ composeApp/src/androidMain/kotlin/dev/tcode/thinmpk/repository/AlbumRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package dev.tcode.thinmpk.repository
+
+import android.provider.MediaStore
+import dev.tcode.thinmpk.model.AlbumModel
+
+class AlbumRepositoryImpl : MediaStoreRepository<AlbumModel>(
+    MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
+    arrayOf(
+        MediaStore.Audio.Albums._ID,
+        MediaStore.Audio.Albums.ALBUM,
+        MediaStore.Audio.Albums.ARTIST,
+    ),
+), AlbumRepository {
+    override fun findAll(): List<AlbumModel> {
+        selection = null
+        selectionArgs = null
+        sortOrder = "${MediaStore.Audio.Albums.ALBUM} ASC"
+
+        return getList()
+    }
+
+    override fun fetch(): AlbumModel {
+        return AlbumModel(
+            id = getId(),
+            name = getAlbumName(),
+            artistName = getArtistName(),
+            imageId = getId(),
+        )
+    }
+
+    private fun getId(): String {
+        return cursor?.getColumnIndex(MediaStore.Audio.Albums._ID)?.let { cursor?.getString(it) } ?: ""
+    }
+
+    private fun getAlbumName(): String {
+        return cursor?.getColumnIndex(MediaStore.Audio.Albums.ALBUM)?.let { cursor?.getString(it) } ?: ""
+    }
+
+    private fun getArtistName(): String {
+        return cursor?.getColumnIndex(MediaStore.Audio.Albums.ARTIST)?.let { cursor?.getString(it) } ?: ""
+    }
+}
