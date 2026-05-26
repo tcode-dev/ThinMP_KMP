68b0e7472cc5c940fd37c69a53abdba65fdfdeaa
tcode <tk@tcode.dev>
2026-05-26 12:05:05 +0900

    Add StyleConstant class for UI styling constants

---
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/constant/StyleConstant.kt
@@ -0,0 +1,23 @@
+package dev.tcode.thinmpk.constant
+
+class StyleConstant {
+    companion object {
+        const val BUTTON_SIZE = 50
+        const val DIVIDER_HEIGHT = 0.5
+        const val FONT_HUGE = 30
+        const val FONT_LARGE = 24
+        const val FONT_MEDIUM = 16
+        const val FONT_SMALL = 14
+        const val FONT_TINY = 12
+        const val GRID_MIN_SPAN_COUNT = 2
+        const val GRID_SPAN_BASE_SIZE = 200
+        const val ICON_SIZE = 24
+        const val IMAGE_CORNER_SIZE = 4
+        const val IMAGE_SIZE = 40
+        const val PADDING_LARGE = 20
+        const val PADDING_MEDIUM = 15
+        const val PADDING_SMALL = 10
+        const val PADDING_TINY = 5
+        const val ROW_HEIGHT = 50
+    }
+}
