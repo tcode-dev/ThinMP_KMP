02bfc789a25a89353aa8c78c8b467cf32e41396b
tcode <tk@tcode.dev>
2026-06-08 00:33:39 +0900

    Refactor PrimaryTitle and SecondaryTitle components to use width instead of fillMaxWidth; remove deprecated padding

--- composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/text/PrimaryTitle.kt
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/text/PrimaryTitle.kt
@@ -1,8 +1,6 @@
 package dev.tcode.thinmpk.view.text
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -11,14 +9,14 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.tcode.thinmpk.constant.StyleConstant
+import dev.tcode.thinmpk.view.util.screenWidth
 
 @Composable
 fun PrimaryTitle(text: String) {
     PlainText(
         text = text,
-        color = MaterialTheme.colorScheme.primary,
         textAlign = TextAlign.Center,
         style = TextStyle(fontSize = StyleConstant.FONT_MEDIUM.sp, fontWeight = FontWeight.Bold),
-        modifier = Modifier.fillMaxWidth().padding(horizontal = (StyleConstant.BUTTON_SIZE + StyleConstant.PADDING_TINY).dp),
+        modifier = Modifier.width(screenWidth() - StyleConstant.BUTTON_SIZE.dp * 2 - StyleConstant.PADDING_TINY.dp * 2),
     )
 }
--- composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/text/SecondaryTitle.kt
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/text/SecondaryTitle.kt
@@ -1,8 +1,7 @@
 package dev.tcode.thinmpk.view.text
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -10,14 +9,14 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.tcode.thinmpk.constant.StyleConstant
+import dev.tcode.thinmpk.view.util.screenWidth
 
 @Composable
 fun SecondaryTitle(text: String) {
     PlainText(
         text = text,
-        color = MaterialTheme.colorScheme.secondary,
         textAlign = TextAlign.Center,
         style = TextStyle(fontSize = StyleConstant.FONT_MEDIUM.sp),
-        modifier = Modifier.fillMaxWidth().padding(horizontal = (StyleConstant.BUTTON_SIZE + StyleConstant.PADDING_TINY).dp),
+        modifier = Modifier.width(screenWidth() - StyleConstant.BUTTON_SIZE.dp * 2 - StyleConstant.PADDING_TINY.dp * 2),
     )
 }
