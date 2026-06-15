f682456f9e76e197e60e7b3144d1936cf4299a6b
tcode <tk@tcode.dev>
2026-06-16 08:47:18 +0900

    Add MiniPlayerLayout to SongsPage for improved layout structure

---
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/layout/MiniPlayerLayout.kt
@@ -0,0 +1,18 @@
+package dev.tcode.thinmpk.view.layout
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import dev.tcode.thinmpk.view.player.MiniPlayer
+
+@Composable
+fun MiniPlayerLayout(content: @Composable (() -> Unit)) {
+    Box(Modifier.fillMaxSize()) {
+        content()
+        Box(Modifier.align(Alignment.BottomCenter)) {
+            MiniPlayer()
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/page/SongsPage.kt
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/page/SongsPage.kt
@@ -1,22 +1,19 @@
 package dev.tcode.thinmpk.view.page
 
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import dev.tcode.thinmpk.view.collapsingAppBar.ListCollapsingAppBar
+import dev.tcode.thinmpk.view.layout.MiniPlayerLayout
 import dev.tcode.thinmpk.view.listItem.SongListItem
-import dev.tcode.thinmpk.view.player.MiniPlayer
 import dev.tcode.thinmpk.viewmodel.SongsViewModel
 
 @Composable
@@ -29,7 +26,7 @@ fun SongsPage(
         viewModel.load()
     }
 
-    Box(Modifier.fillMaxSize()) {
+    MiniPlayerLayout {
         ListCollapsingAppBar("Songs") {
             itemsIndexed(uiState.songs) { index, song ->
                 SongListItem(song, Modifier.pointerInput(index) {
@@ -39,9 +36,6 @@ fun SongsPage(
                     )
                 })
             }
-        }
-        Box(Modifier.align(Alignment.BottomCenter)) {
-            MiniPlayer()
         }
     }
 }
