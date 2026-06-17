402753f8fac63c84ad5ce6a294b3dbbc2e0ad76d
tcode <tk@tcode.dev>
2026-06-18 07:41:12 +0900

    Refactor SongListItem to apply consistent height using StyleConstant

--- composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/listItem/SongListItem.kt
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/listItem/SongListItem.kt
@@ -3,12 +3,14 @@ package dev.tcode.thinmpk.view.listItem
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import dev.tcode.thinmpk.constant.StyleConstant
 import dev.tcode.thinmpk.model.SongModel
 import dev.tcode.thinmpk.view.image.ArtworkImage
 import dev.tcode.thinmpk.view.text.PrimaryText
@@ -16,7 +18,9 @@ import dev.tcode.thinmpk.view.text.SecondaryText
 
 @Composable
 fun SongListItem(song: SongModel, modifier: Modifier = Modifier) {
-    Column {
+    Column(
+        Modifier.height(StyleConstant.ROW_HEIGHT.dp)
+    ) {
         Row(
             modifier = modifier
                 .fillMaxWidth()
