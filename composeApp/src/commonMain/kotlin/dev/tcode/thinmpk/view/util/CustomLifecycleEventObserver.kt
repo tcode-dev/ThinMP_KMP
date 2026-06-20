d33c36286b3db10374e92d4d25042ab2456bf17a
tcode <tk@tcode.dev>
2026-06-20 09:34:04 +0900

    Add CustomLifecycleEventObserver for lifecycle event handling in Compose

---
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/util/CustomLifecycleEventObserver.kt
@@ -0,0 +1,36 @@
+package dev.tcode.thinmpk.view.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
+
+interface CustomLifecycleEventObserverListener {
+    fun onResume() {}
+    fun onStop() {}
+    fun onDestroy() {}
+}
+
+@Composable
+fun CustomLifecycleEventObserver(listener: CustomLifecycleEventObserverListener) {
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    val currentOnEvent by rememberUpdatedState(listener)
+
+    DisposableEffect(key1 = lifecycle, key2 = currentOnEvent) {
+        val lifecycleObserver = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_RESUME -> listener.onResume()
+                Lifecycle.Event.ON_STOP -> listener.onStop()
+                Lifecycle.Event.ON_DESTROY -> listener.onDestroy()
+                else -> {}
+            }
+        }
+        lifecycle.addObserver(lifecycleObserver)
+        onDispose {
+            lifecycle.removeObserver(lifecycleObserver)
+        }
+    }
+}
