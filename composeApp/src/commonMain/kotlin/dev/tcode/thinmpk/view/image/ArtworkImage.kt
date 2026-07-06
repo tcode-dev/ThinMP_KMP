5db1e4aa3d45191b97f4de53cda07510f823a8ea
tcode <tk@tcode.dev>
2026-07-06 18:13:53 +0900

    Update ArtworkImage to use a resource-based placeholder instead of MaterialTheme color

--- composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/image/ArtworkImage.kt
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/view/image/ArtworkImage.kt
@@ -1,19 +1,20 @@
 package dev.tcode.thinmpk.view.image
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import coil3.compose.AsyncImage
 import dev.tcode.thinmpk.model.ArtworkModel
+import org.jetbrains.compose.resources.painterResource
+import thinmpk.composeapp.generated.resources.Res
+import thinmpk.composeapp.generated.resources.song_dark
 
 @Composable
 fun ArtworkImage(
     imageId: String,
     modifier: Modifier = Modifier,
 ) {
-    val placeholder = ColorPainter(MaterialTheme.colorScheme.surfaceVariant)
+    val placeholder = painterResource(Res.drawable.song_dark)
 
     AsyncImage(
         model = ArtworkModel(id = imageId),
