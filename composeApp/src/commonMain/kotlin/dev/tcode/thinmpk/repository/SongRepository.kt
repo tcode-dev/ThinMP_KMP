76df8ce5e4c7459e983deeef26463e7be427968b
tcode <tk@tcode.dev>
2026-07-18 13:08:00 +0900

    Add findByIds method to SongRepository interface

--- composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/repository/SongRepository.kt
+++ composeApp/src/commonMain/kotlin/dev/tcode/thinmpk/repository/SongRepository.kt
@@ -5,6 +5,7 @@ import dev.tcode.thinmpk.model.SongModel
 interface SongRepository {
     fun findAll(): List<SongModel>
     fun findById(id: String): SongModel?
+    fun findByIds(ids: List<String>): List<SongModel>
     fun findByAlbumId(albumId: String): List<SongModel>
     fun findByArtistId(artistId: String): List<SongModel>
 }
